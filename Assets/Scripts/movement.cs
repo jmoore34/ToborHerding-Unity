4558e99ce5f4d3b4f41e41eed3942147a07cedf0
Hogan S <37481947+Hogan-Dazs@users.noreply.github.com>
2021-04-28 23:03:08 -0500

    Add reset button

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class movement : MonoBehaviour
 {
@@ -9,13 +10,17 @@ public class movement : MonoBehaviour
     public float speed;
     public float rotationSpeed;
     public InputAction joystick;
+    public InputAction reset;
     private Vector3 moveDirection = Vector3.zero;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
+        anim = gameObject.GetComponent<Animator>();
         characterController = GetComponent<CharacterController>();
         joystick.Enable();
+        reset.Enable();
     }
 
     // Update is called once per frame
@@ -31,7 +36,17 @@ public class movement : MonoBehaviour
         if (moveDirection != Vector3.zero)
         {
             transform.forward = moveDirection;
+            if(!anim.GetBool("Walk_Anim"))
+                anim.SetBool("Walk_Anim", true);
+        }
+        else if (anim.GetBool("Walk_Anim"))
+        {
+            anim.SetBool("Walk_Anim", false);
         }
 
+        if (reset.triggered)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
