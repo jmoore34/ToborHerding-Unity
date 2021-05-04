18b2167a12a65a7f4376c8d91346ff0e930a4432
Jonathan Moore <jtm170330@utdallas.edu>
2021-05-03 21:58:29 -0500

    Add score controller

---
+++ Assets/ScoreController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Google.Protobuf.WellKnownTypes;
+using TMPro;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    // singleton design pattern
+    public static ScoreController Instance { get; private set; }
+    
+
+    public int MaxTobors = 6;
+
+    private TextMeshProUGUI textMesh;
+    private int _score;
+
+    public int Score
+    {
+        get { return _score; }
+        set
+        {
+            _score = value;
+            textMesh.text = $"{_score}/{MaxTobors} collected";
+        }
+    }
+
+    private void Awake()
+    {
+        Instance = this;
+        textMesh = GetComponent<TextMeshProUGUI>();
+
+        Score = 0;
+    }
+    
+}
