145bf2da9be84af6991846123986fcd4fd0b3b55
Jonathan Moore <jtm170330@utdallas.edu>
2021-05-03 21:40:51 -0500

    Add timer

---
+++ Assets/TimerController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Google.Protobuf.WellKnownTypes;
+using TMPro;
+using UnityEngine;  
+
+public class TimerController : MonoBehaviour
+{
+    // Singleton pattern
+    public static TimerController Instance { get; private set; }
+
+
+    public Stopwatch Stopwatch;
+
+    private TextMeshProUGUI textMesh;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMesh = GetComponent<TextMeshProUGUI>();
+
+        Stopwatch = new Stopwatch();
+        Stopwatch.Restart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textMesh.text = Stopwatch.Elapsed.ToString("mm':'ss':'ff");
+    }
+}
